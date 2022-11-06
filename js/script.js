1956e12df483624fea2cae4dbf45264ee2a62c67
Ognyan Stoyanov <o.r.stoyanov@gmail.com>
2022-11-07 00:53:16 +0200

    07.11/00:53

--- js/script.js
+++ js/script.js
@@ -10,9 +10,12 @@ $(document).ready(function () {
      $('#btn-three').click(function () { 
         window.location.href='#skills';
      });
-     $('#btn-four').click(function () { 
-        window.location.href='#';
+     $('#btn-five').click(function () { 
+        window.location.href='#contacts';
      });
+     $('#btn-four').click(function () { 
+      window.open=('https://drive.google.com/file/d/1kKggijEatPnLp_risrHXrERn9YSgUnvd/view?usp=share_link');   
+   });
 });
 
 // progress-bar
@@ -20,6 +23,7 @@ $(document).ready(function(){
 	$('.skill-bar').each(function(){
 		$(this).find('.bar').animate({
 			width:$(this).attr('data-percent')
-		},4000);
+		},10000);
 	});
 });
+
